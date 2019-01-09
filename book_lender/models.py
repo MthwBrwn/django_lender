69c7d9d22faa8a591c8685175eb521f8a9486e2b
mthwbrwn <mthwbrwn@gmail.com>
2019-01-08 16:13:03 -0800

    models set up

--- book_lender/models.py
+++ book_lender/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    """
+    """
+    cover_image   #: An image field
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    year = models.CharField(max_length=4)
+    STATES = [
+        ('checked-in', 'Checked-In')
+        ('checked-out', 'Checked-Out')
+    ]
+    status = models.CharField(max_length=48, default='checked-in', choices=STATES)
+    date_added  #: Auto-generated date field
+    last_borrowed  #: Auto-updated date field
+
+    def __repr__(self):
+        """
+        """
+        pass
+
+    def __str__(self):
+        """
+        """
+        pass
