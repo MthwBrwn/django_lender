26a05aae1de1caa2a416c52ead09ec4639a28d9d
mthwbrwn <mthwbrwn@gmail.com>
2019-01-08 21:55:27 -0800

    unable to make images work

--- book_lender/models.py
+++ book_lender/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Book(models.Model):
+    """ This is the class Book which is the model for our book object
     """
-    """
-    # cover_image   #: An image field
+    # cover_image = models.ImageField()
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     year = models.CharField(max_length=4)
@@ -13,15 +13,15 @@ class Book(models.Model):
         ('checked_out', 'Checked Out'),
     ]
     status = models.CharField(max_length=48, default='checked_in', choices=STATES)
-    # date_added  #: Auto-generated date field
-    # last_borrowed  #: Auto-updated date field
+    # date_added = models.DateTimeField(auto_now_add=True)
+    # last_borrowed = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
+        """ Repr returns the title and then the date added and last borrowed date
         """
-        """
-        return f'{self.title}, {self.author}, {self.year}, {self.status} '
+        return f'{self.title}, {self.author}, {self.year}, {self.status},'
 
     def __str__(self):
-        """
+        """ str gives back basic information about the class objects
         """
         return f'{self.title}, {self.author}, {self.year}, {self.status} '
