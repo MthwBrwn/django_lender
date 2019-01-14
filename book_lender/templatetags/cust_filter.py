df42432274b1b4febcd57b1b23b9a47a5a114522
mthwbrwn <mthwbrwn@gmail.com>
2019-01-13 19:18:08 -0800

    custom filter works

--- book_lender/templatetags/cust_filter.py
+++ book_lender/templatetags/cust_filter.py
@@ -1,17 +1,22 @@
-from dhango.utils import timezone
+from django.utils import timezone
+from django import template
+
+register = template.Library()
 
 
+@register.filter
 def get_date_string(val):
-    """
+    """ This is a custom filter that determines the date change was
+    made to list with current time and returns appropriate message.
     """
     now_aware = timezone.now()
-    timediff = val - now_aware
+    timediff = now_aware - val
 
     if timediff.days == 0:
         return 'Today'
 
     elif timediff.days == 1:
-        return "Tomorrow"
+        return "Yesterday"
 
     elif timediff.days > 1:
-        return f'In {timediff.days} days'
+        return f' {timediff.days} days ago '
